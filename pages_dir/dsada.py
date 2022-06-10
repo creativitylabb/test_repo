7a3b82e0ff2352548bae58f200400f28b4cd8edc
creativitylabb <iuliana.stroia97@gmail.com>
2022-06-10 16:58:16 +0300

    update

--- pages_dir/dsada.py
+++ pages_dir/dsada.py
@@ -4,5 +4,5 @@ clean_df = pd.read_csv('last_step_pagination_110422.csv', infer_datetime_format=
 
 # df = df.drop('Unnamed: 0', axis=1)
 
-clean_df['TimeStamp'] = pd.to_datetime(clean_df['TimeStamp'], format="%Y-%m-%d %H:%M:%S")
+clean_df['TimeStamp'] = pd.to_datetime(clean_df['TimeStamp'])
 print(clean_df)
